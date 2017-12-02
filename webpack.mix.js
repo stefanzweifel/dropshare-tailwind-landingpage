b74586837090f6674a3bac1950a089aa511d7994
Stefan Zweifel <hello@stefanzweifel.io>
2017-12-02 13:28:59 +0100

    Inline CSS with HtmlWebpackPlugin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,38 +1,51 @@
 const { mix } = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
+const purgeCss = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
 
-var tailwindcss = require('tailwindcss');
+mix.disableNotifications();
 
+/**
+ * Build CSS with Tailwind
+ */
 mix.postCss('src/main.css', 'dist/css', [
   tailwindcss('./tailwind.js'),
 ]);
 
-mix.disableNotifications();
 
-
-let purgeCss = require('purgecss-webpack-plugin');
-let glob = require('glob-all');
-
-if (mix.inProduction()) {
-
-    mix.webpackConfig({
-        plugins: [
-            new purgeCss({
-                paths: glob.sync(
-                    [
-                        path.join(__dirname, './dist/**/*.html'),
-                    ]
-                ),
-                extractors: [
-                    {
-                        extractor: class {
-                            static extract(content) {
-                                return content.match(/[A-z0-9-:\/]+/g)
-                            }
-                        },
-                        extensions: ['html'],
-                    }
+/**
+ * Additional Webpack Configuration
+ * 
+ * - Remove not used CSS with purgecss
+ * - Inline CSS with HtmlWebpackPlugin
+ */
+mix.webpackConfig({
+    plugins: [
+        new purgeCss({
+            paths: glob.sync(
+                [
+                    path.join(__dirname, './dist/**/*.html'),
                 ]
-            })
-        ]
-    })
-}
+            ),
+            extractors: [
+                {
+                    extractor: class {
+                        static extract(content) {
+                            return content.match(/[A-z0-9-:\/]+/g)
+                        }
+                    },
+                    extensions: ['html'],
+                }
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            template: 'dist/template-index.html',
+            filename: 'dist/index.html',
+        }),
+        new StyleExtHtmlWebpackPlugin({
+            minify: true
+        }),
+    ]
+})
